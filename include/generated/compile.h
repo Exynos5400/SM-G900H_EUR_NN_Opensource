d3707831cd705c2b2e86b4fcceed6cf71fa63788
aditheking <aupreti72@gmail.com>
2017-04-10 17:21:02 +0000

    Port Diag

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 0 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Mon Apr 10 16:53:16 UTC 2017"
+#define UTS_VERSION "#0 SMP PREEMPT Mon Apr 10 17:03:10 UTC 2017"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "daring-machine-v2"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.9-g5ffe156-dirty"
+#define UTS_RELEASE "3.10.9-g08f417b-dirty"
